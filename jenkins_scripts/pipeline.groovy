450775a70c936316995b0bd528e34f72f2fa59b5
Pushpinder Rana <er.pushpinderrana@gmail.com>
2020-10-02 13:41:44 -0400

    Update pipeline.groovy

--- jenkins_scripts/pipeline.groovy
+++ jenkins_scripts/pipeline.groovy
@@ -3,10 +3,10 @@ node {
     git branch: "master", url: "https://github.com/erpushpinderrana/jenkins-terraform-aws.git"
   }
   stage('Terraform Initialize') {
-    sh "sh terraform init -input=false -var-file=vars.tfvars"
+    sh "terraform init -input=false -var-file=vars.tfvars"
   }
   stage('Terraform Plan') {
-    sh "sh terraform plan"
+    sh "terraform plan"
   }
   stage('Terraform Apply') {
     sh "terraform apply -auto-approve -input=false -var-file=vars.tfvars"
